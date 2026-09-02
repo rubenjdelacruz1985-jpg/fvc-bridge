665639ed6f44045312d3dd74dece624104d2a3c1
rubenjdelacruz1985-jpg <rubenjdelacruz1985@gmail.com>
2026-09-02 10:47:43 -0700

    v1.15.0: publish-post accepts template (_wp_page_template)

--- fvc-bridge.php
+++ fvc-bridge.php
@@ -2,14 +2,14 @@
 /**
  * Plugin Name: FVC Bridge
  * Description: Token-authenticated REST bridge + self-update channel for Find Vancouver Clinics.
- * Version: 1.14.0
+ * Version: 1.15.0
  * Author: Ruben de la Cruz
  * Update URI: https://github.com/rubenjdelacruz1985-jpg/fvc-bridge
  */
 
 if ( ! defined('ABSPATH') ) exit;
 
-define('FVC_BRIDGE_VERSION',    '1.14.0');
+define('FVC_BRIDGE_VERSION',    '1.15.0');
 define('FVC_BRIDGE_SLUG',       'fvc-bridge');
 define('FVC_BRIDGE_BASENAME',   plugin_basename(__FILE__)); // fvc-bridge/fvc-bridge.php
 define('FVC_BRIDGE_MANIFEST',   'https://github.com/rubenjdelacruz1985-jpg/fvc-bridge/releases/latest/download/manifest.json');
@@ -982,6 +982,9 @@ function fvc_bridge_rest_publish_post($req) {
     if ( ! empty($p['category']) ) {
         $cat = is_numeric($p['category']) ? (int) $p['category'] : get_cat_ID(sanitize_text_field($p['category']));
         if ( $cat ) wp_set_post_categories($post_id, array($cat));
+    }
+    if ( isset($p['template']) ) {
+        update_post_meta($post_id, '_wp_page_template', sanitize_text_field($p['template']));
     }
     if ( ! empty($p['meta_description']) && function_exists('update_post_meta') ) {
         update_post_meta($post_id, 'rank_math_description', sanitize_text_field($p['meta_description']));
